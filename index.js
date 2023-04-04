13729fe24ac592b2818ca3acfa396a8860452b19
Lipton Barua <liptonbarua274@gmail.com>
2023-04-04 13:12:01 +0600

    project

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ require('dotenv').config()
 app.use(cors())
 app.use(express.json())
 
-app.get('/', (req, res)=>{
-    res.send('assianment project is running')
+app.get('/', (req, res) => {
+  res.send('assianment project is running')
 })
 
 
@@ -18,63 +18,90 @@ app.get('/', (req, res)=>{
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ebocqiq.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run(){
-    try{
-     const dataCollection = client.db('assianment').collection('services');
-     const orderCollection = client.db('assianment').collection('orders');
+async function run() {
+  try {
+    const dataCollection = client.db('assianment').collection('services');
+    const orderCollection = client.db('assianment').collection('orders');
 
-     app.get('/service', async(req, res)=>{
-        const page =parseInt(req.query.page);
-        const size =parseInt(req.query.size)
-        const query ={};
-        const cursor = dataCollection.find(query);
-        const service = await cursor.skip(page*size).limit(size).toArray();
-        const count = await dataCollection.estimatedDocumentCount();
-        res.send({count, service})
-     })
+    app.get('/service', async (req, res) => {
+      const page = parseInt(req.query.page);
+      const size = parseInt(req.query.size)
+      const query = {};
+      const cursor = dataCollection.find(query);
+      const service = await cursor.skip(page * size).limit(size).toArray();
+      const count = await dataCollection.estimatedDocumentCount();
+      res.send({ count, service })
+    })
 
-       app.get('/service/:id', async(req, res)=>{
-        const id =req.params.id;
-        const query = {_id:ObjectId(id)}
-        const services = await dataCollection.findOne(query);
-        res.send(services);
-     })
-
-     app.post('/service', async(req, res)=>{
-        const result = await dataCollection.insertOne(req.body);
-     })
-
-     app.post('/orders', async(req, res)=>{
-        const order = req.body;
-        const result = await orderCollection.insertOne(order);
-        res.send(result); 
-     })
-     app.get('/orders', async(req, res)=>{
-      let query = {}
-      if(req.query.email){
-        query={
-            email: req.query.email
-        }
-      }
-      const cursor = orderCollection.find(query);
-      const result = await cursor.toArray();
+    app.get('/cards', async (req, res) => {
+      const query = {}
+      const result = await dataCollection.find(query).toArray();
       res.send(result)
-     })
+    })
 
-     app.delete('/orders/:id', async(req, res)=>{
+    app.get('/service/:id', async (req, res) => {
       const id = req.params.id;
-      const query ={_id: ObjectId(id)};
+      const query = { _id: ObjectId(id) }
+      const services = await dataCollection.findOne(query);
+      res.send(services);
+    })
+
+    app.post('/service', async (req, res) => {
+      const result = await dataCollection.insertOne(req.body);
+      if (result.insertedId) {
+        res.send({
+          success: true,
+          message: `Successfully created the ${req.body.name} with id ${result.insertedId}`,
+        });
+      }
+    })
+
+    // app.put('/rating', async (req, res) => {
+    //   const serviceEmail = req.query._id;
+    //   const body = req.body;
+    //   const {_id, name, email, message, phote, rating, date } = body;
+    //   const filter = { _id: serviceEmail }
+    //   const option = { upsert: true }
+    //   const updatedDoc = {
+    //     $set: {
+    //       name, email, message, phote, rating, date
+    //     }
+    //   }
+    //   const result = await dataCollection.updateOne(filter, updatedDoc, option)
+    //   res.send(result)
+    // })
+
+    app.post('/rating', async (req, res) => {
+      const order = req.body;
+      const result = await orderCollection.insertOne(order);
+      res.send(result);
+    })
+    app.get('/rating', async (req, res) => {
+      const query = {};
+      const result = await orderCollection.find().toArray();
+      res.send(result)
+    })
+    app.get('/myReview', async (req, res) => {
+      const email = req.query.email;
+      let query = { email: email }
+      const result = await orderCollection.find(query).toArray();
+      res.send(result)
+    })
+
+    app.delete('/myReview/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
       const result = await orderCollection.deleteOne(query);
       res.send(result);
-  })
-    }
-    finally{
+    })
+  }
+  finally {
 
-    }
+  }
 
 }
-run().catch(error=>console.log(error))
+run().catch(error => console.log(error))
 
-app.listen(port, (req, res)=>{
-    console.log(`assiament is running ${port}`)
+app.listen(port, (req, res) => {
+  console.log(`assiament is running ${port}`)
 })
